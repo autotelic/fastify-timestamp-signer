043d7aa7333f13204a22c78b4e28a00ac6fe9043
Meg Hein <64171419+meghein@users.noreply.github.com>
2024-01-05 10:50:08 -0800

    feat: more types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,39 @@
-declare module '@autotelic/fastify-timestamp-signer' {
+import { FastifyPluginCallback } from 'fastify';
 
-  function fastifyTimestampSigner(fastify: any, options: any): void;
+type FastifyTimestampSigner = FastifyPluginCallback<fastifyTimestampSigner.TimestampSignerOptions>;
 
-  export = fastifyTimestampSigner;
+declare module 'fastify' {
+  interface FastifyInstance {
+      sign(string: string, options?: fastifyTimestampSigner.SignOptions): Promise<string>;
+      validate(signedString: string, maxAge?: number, options?: fastifyTimestampSigner.ValidateOptions): Promise<boolean>;
+  }
 }
+
+declare namespace fastifyTimestampSigner {
+    export interface SignOptions {
+        salt: string;
+        timestamp?: number;
+    }
+
+    export interface ValidateOptions {
+        salt: string;
+        validateTime?: number;
+    }
+
+    export interface TimestampSignerOptions {
+        secret: string;
+        algorithm?: string;
+        delimiter?: string;
+        encoding?: string;
+    }
+
+    export const fastifyTimestampSigner: FastifyTimestampSigner;
+    export { fastifyTimestampSigner as default };
+}
+
+declare function fastifyTimestampSigner(
+  ...params: Parameters<FastifyTimestampSigner>
+): ReturnType<FastifyTimestampSigner>;
+
+export = fastifyTimestampSigner;
+
